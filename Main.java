c00ba392163422910b2aa097801bbfc2d0b0e4ab
arikakira <samanthal242@nycstudents.net>
2025-05-02 15:39:32 +0000

    Update

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main
 {
     public static void main(String[] args) {
-        
+        String[] wordNums = {"one", "two", "three"};
+        WordPairList = exampleOne = new WordPairList(wordNums);
     }
 }
--- WordPairList.java
+++ WordPairList.java
@@ -2,13 +2,19 @@
 public class WordPairList {
     /** The list of word pairs, initialized by the constructor. */
     private ArrayList<WordPair> allPairs;
+    allPairs = new ArrayList<WordPair>();
 
     /**
      * Constructs a WordPairList object as described in part (a).
      * Precondition: words.length >= 2
      */
     public WordPairList(String[] words) {
-        /* to be implemented in part (a) */ }
+        for(int i=0; i<words.length; i++) {
+            for(int k=i+1; k<words.length; k++) {
+
+            }
+        }
+    }
 
     /**
      * Returns the number of matches as described in part (b).
